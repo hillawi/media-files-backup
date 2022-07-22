aa45b61e2d8e18f34bf827101bfe8af6fb02a5a0
Ahmed Hillawi <hillawi@gmail.com>
2022-07-22 15:39:12 +0200

    feature/device-configuration: bash script - Reading the devices from the configuration file

--- bin/launch_backup.sh
+++ bin/launch_backup.sh
@@ -3,74 +3,78 @@
 ###################################################################
 # Script Name	: launch_backup.sh
 # Description	: Launch the backup process
-# Args        : The device ID and the media type (IMG, VID, etc.)
+# Args        : The source device ID, the target device ID and the
+#               media type (IMG, VID, etc.)
 # Author      : Ahmed Abdeen
+# Last update : 2022/07/22
 ###################################################################
+
+# Debug option, comment out if not needed
+#set -x
 
 # Error codes
 ERROR_INPUT_PARAM_INVALID=1
-ERROR_DEVICE_UNKNOWN=2
-ERROR_MEDIA_TYPE_UNKNOWN=3
-ERROR_INPUT_FOLDER_NOTFOUND=4
-ERROR_OUTPUT_FOLDER_NOTFOUND=5
+ERROR_MEDIA_TYPE_UNKNOWN=2
+ERROR_INPUT_FOLDER_NOTFOUND=3
+ERROR_OUTPUT_FOLDER_NOTFOUND=4
+ERROR_BAD_DEVICE_CONF=5
 
 if [[ $# -ne 3 ]]; then
-  echo "The source and target devices IDs (ex: 6t, 7t) and the media type (IMG or VID) are required"
+  echo "The source device ID, the target device ID and the media type (IMG or VID) are required"
   exit $ERROR_INPUT_PARAM_INVALID
 fi
 
 BIN_DIR=$MFB_BIN_DIR
+DEVICES_CONF_FILE="$MFB_CONF_DIR/devices.json"
 
 declare -A INPUT_PATHS_PER_MEDIA_TYPE
 declare -A OUTPUT_PATHS_PER_MEDIA_TYPE
 
 SOURCE_DEVICE_ID=$1
 TARGET_DEVICE_ID=$2
-
-if [[ 1 -eq 1 ]]; then
-  printf "Done"
-  exit 0
-fi
-
-# TODO Use switch, extract the list of device IDs
-if [[ "$SOURCE_DEVICE_ID" == "6t" ]]; then
-  INPUT_PATHS_PER_MEDIA_TYPE[IMG]=$MFB_INPUT_ONEPLUS6T_IMG
-  INPUT_PATHS_PER_MEDIA_TYPE[VID]=$MFB_INPUT_ONEPLUS6T_VID
-
-  OUTPUT_PATHS_PER_MEDIA_TYPE[IMG]=$MFB_OUTPUT_ONEPLUS6T_IMG
-  OUTPUT_PATHS_PER_MEDIA_TYPE[VID]=$MFB_OUTPUT_ONEPLUS6T_VID
-elif [[ "$SOURCE_DEVICE_ID" == "7t" ]]; then
-  INPUT_PATHS_PER_MEDIA_TYPE[IMG]=$MFB_INPUT_ONEPLUS7T_IMG
-  INPUT_PATHS_PER_MEDIA_TYPE[VID]=$MFB_INPUT_ONEPLUS7T_VID
-
-  OUTPUT_PATHS_PER_MEDIA_TYPE[IMG]=$MFB_OUTPUT_ONEPLUS7T_IMG
-  OUTPUT_PATHS_PER_MEDIA_TYPE[VID]=$MFB_OUTPUT_ONEPLUS7T_VID
-else
-  echo "Unknown device ID. Supported device IDs are 6t and 7t."
-  exit $ERROR_DEVICE_UNKNOWN
-fi
-
 MEDIA_TYPE=$3
 FILE_SUFFIX=""
 
+# Read the devices list from the configuration file
+devices=$(cat "$DEVICES_CONF_FILE");
+sourceDeviceType=$(echo "$devices" | jq -r --arg SOURCE_DEVICE_ID "$SOURCE_DEVICE_ID" '.[] | select(.id==$SOURCE_DEVICE_ID) | .type');
+targetDeviceType=$(echo "$devices" | jq -r --arg TARGET_DEVICE_ID "$TARGET_DEVICE_ID" '.[] | select(.id==$TARGET_DEVICE_ID) | .type');
+
+# Devices validation
+if [[ "$sourceDeviceType" != "source" ]]; then
+  echo "The source device is not of type source"
+  exit $ERROR_BAD_DEVICE_CONF
+fi
+
+if [[ "$targetDeviceType" != "target" ]]; then
+  echo "The target device is not of type target"
+  exit $ERROR_BAD_DEVICE_CONF
+fi
+
+sourceDeviceMountPath=""
+# TODO add the target device mount path to the source devices subPaths
+
 if [[ "$MEDIA_TYPE" == "IMG" ]]; then
   FILE_SUFFIX="IMG_*.jpg"
+  sourceDeviceMountPath=$(echo "$devices" | jq -r --arg SOURCE_DEVICE_ID "$SOURCE_DEVICE_ID" '.[] | select(.id==$SOURCE_DEVICE_ID) | .mountPath.img');
+  sourceDeviceTargetSubPath=$(echo "$devices" | jq -r --arg SOURCE_DEVICE_ID "$SOURCE_DEVICE_ID" '.[] | select(.id==$SOURCE_DEVICE_ID) | .targetSubPath.img');
 elif [[ "$MEDIA_TYPE" == "VID" ]]; then
   FILE_SUFFIX="VID_*.mp4"
+  sourceDeviceMountPath=$(echo "$devices" | jq -r --arg SOURCE_DEVICE_ID "$SOURCE_DEVICE_ID" '.[] | select(.id==$SOURCE_DEVICE_ID) | .mountPath.vid');
+  sourceDeviceTargetSubPath=$(echo "$devices" | jq -r --arg SOURCE_DEVICE_ID "$SOURCE_DEVICE_ID" '.[] | select(.id==$SOURCE_DEVICE_ID) | .targetSubPath.vid');
 else
   echo "Unknown media type $MEDIA_TYPE. Known types are IMG and VID."
   exit $ERROR_MEDIA_TYPE_UNKNOWN
 fi
 
-INPUT_FOLDER=${INPUT_PATHS_PER_MEDIA_TYPE["$MEDIA_TYPE"]}
-DEST_FOLDER=${OUTPUT_PATHS_PER_MEDIA_TYPE["$MEDIA_TYPE"]}
+targetDeviceMountPath=$(echo "$devices" | jq -r --arg TARGET_DEVICE_ID "$TARGET_DEVICE_ID" '.[] | select(.id==$TARGET_DEVICE_ID) | .mountPath')"/$sourceDeviceTargetSubPath"
 
-[[ -d "$INPUT_FOLDER" ]] || {
-  echo "Input folder ($INPUT_FOLDER) does not exist"
+[[ -d "$sourceDeviceMountPath" ]] || {
+  echo "Input folder ($sourceDeviceMountPath) does not exist"
   exit $ERROR_INPUT_FOLDER_NOTFOUND
 }
-[[ -e "$DEST_FOLDER" ]] || {
-  echo "Destination folder ($DEST_FOLDER) does not exist"
+[[ -e "$targetDeviceMountPath" ]] || {
+  echo "Destination folder ($targetDeviceMountPath) does not exist"
   exit $ERROR_OUTPUT_FOLDER_NOTFOUND
 }
 
@@ -78,19 +82,19 @@ printf "\n\n"
 echo "****************** MEDIA FILES BACKUP *******************"
 echo "** DATE     : $(date +"%m-%d-%Y")"
 echo "** DEVICE ID: $SOURCE_DEVICE_ID"
-echo "** FROM     : $INPUT_FOLDER"
-echo "** TO       : $DEST_FOLDER"
+echo "** FROM     : $sourceDeviceMountPath"
+echo "** TO       : $targetDeviceMountPath"
 echo "*********************************************************"
 printf "\n"
 
-cd "$DEST_FOLDER" || {
+cd "$targetDeviceMountPath" || {
   echo "Cannot change directory. Exiting."
   exit 6
 }
 
 numberOfCopiedFiles=0
 
-bash "$BIN_DIR/do_backup.sh" "$INPUT_FOLDER" "$FILE_SUFFIX" "$MEDIA_TYPE" "$SOURCE_DEVICE_ID" &
+bash "$BIN_DIR/do_backup.sh" "$sourceDeviceMountPath" "$FILE_SUFFIX" "$MEDIA_TYPE" "$SOURCE_DEVICE_ID" &
 
 childPid=$!
 
