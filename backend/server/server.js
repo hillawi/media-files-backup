f19eeee206ea3fe37869329f19ddb5a25c9d85ba
Ahmed Abdeen <abdeenahmed2002@hotmail.com>
2022-08-19 20:52:49 +0200

    First release preparation (#4)
    * Device configuration (#3)
    
    * Updated README
    
    * feature/device-configuration: Added devices.json. Updated env variables
    
    * feature/device-configuration: Backend - Added package.json
    
    * feature/device-configuration: Using express js
    
    * feature/device-configuration: Folders refactoring
    
    * feature/device-configuration: Updated device config
    
    * feature/device-configuration: Frontend update
    
    * feature/device-configuration: Frontend update
    
    * feature/device-configuration: Updated the bash script input args
    
    * feature/device-configuration: gitignore update
    
    * feature/device-configuration: bash script - Reading the devices from the configuration file
    
    * feature/device-configuration: bash script - Reading the desired backup start date from the destination device
    
    * feature/device-configuration: README - Added sequence diagram
    
    * feature/device-configuration: README - Added sequence diagram
    
    * feature/device-configuration: Some few enhancements and error management
    
    Co-authored-by: Ahmed Hillawi <hillawi@gmail.com>
    
    * Using env variables
    
    * Added README for the backend server
    
    * Added README for the backend server
    
    * env fix
    
    * Added frontend jenkins file
    
    * Added frontend jenkins file
    
    * Added frontend jenkins file
    
    * Added frontend jenkins file
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Jenkins configuration update
    
    * Background update
    
    * Background update
    
    * Added some documentation
    
    * Automatic backup launch (#2)
    
    * Updated readme
    
    * Updated README
    
    * Updated devices config
    
    * Added udev rule
    
    * Added automatic launch script
    
    * Added nginx config for the frontend
    
    * Documentation update
    
    * Finding the device type. Write to log file.
    
    * SCript update
    
    * Code refactoring
    
    * Code refactoring
    
    * Added a file watcher
    
    * Updated udev rules
    
    * Updated udev rules
    
    * Updated udev rules
    
    * Updated udev rules
    
    * Updated udev rules
    
    * Updated udev rules
    
    * Updated udev rules
    
    * Documentation update
    
    Co-authored-by: Ahmed Hillawi <hillawi@gmail.com>
    
    * Documentation update
    
    * Documentation update
    
    * Documentation update
    
    * Documentation update
    
    * Documentation update
    
    * replaced "bin" by "system"
    
    * replaced "bin" by "system"
    
    * Directories refactoring
    
    * replaced "bin" by "system"
    
    * Added docker and docker compose files
    
    * Updated docker configurations
    
    * Documentation update
    
    * backend url update
    
    * Documentation update
    
    * Docker image id update
    
    * Doc update
    
    * Doc update
    
    * Doc update
    
    * Doc update
    
    Co-authored-by: Ahmed Hillawi <hillawi@gmail.com>

---
+++ backend/server/server.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const fs = require('fs');
+const {exec} = require("child_process");
+
+const port = 8387;
+const app = express();
+
+app.use(express.json());
+
+const DEVICES_CONF_PATH = process.env.MFB_DEVICES_CONF_PATH;
+const SCRIPT_PATH = process.env.MFB_BIN_DIR + "/" + process.env.MFB_SCRIPT_NAME;
+
+function BackupExecutor() {
+    this.execCommand = function (cmd) {
+        return new Promise((resolve, reject) => {
+            exec(cmd, (error, stdout, stderr) => {
+                if (error) {
+                    console.log('error:', error);
+                    reject(stdout);
+                }
+                if (stderr) {
+                    console.log('stderr:', stderr);
+                    reject(stdout);
+                } else {
+                    console.log('stdout:', stdout);
+                    resolve(stdout);
+                }
+            });
+        });
+    }
+}
+
+async function launchBackup(req, res) {
+    const content = req.body;
+    const mediaType = content.mediaType;
+    const sourceDeviceId = content.sourceDeviceId;
+    const targetDeviceId = content.targetDeviceId;
+
+    const backupExecutor = new BackupExecutor();
+    const cmd = "bash " + SCRIPT_PATH + " " + sourceDeviceId + " " + targetDeviceId + " " + mediaType;
+
+    await backupExecutor.execCommand(cmd)
+        .then((output) => {
+            res.json(JSON.parse('{"output": "' + output.replace(/(\r\n|\r|\n)/g, '<br>') + '"}'));
+        }).catch((err) => {
+            res.status(500).json(JSON.parse('{"error": "' + err.replace(/(\r\n|\r|\n)/g, '<br>') + '"}'));
+        });
+}
+
+function setHeaders(res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader('Access-Control-Max-Age', 2592000);
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Powered-By', 'mfb-server');
+}
+
+app.get('/devices', (req, res) => {
+    setHeaders(res);
+    try {
+        const data = JSON.parse(fs.readFileSync(DEVICES_CONF_PATH, 'utf8'));
+        res.json(data);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+app.options('/launchBackup', (req, res) => {
+    setHeaders(res);
+    res.sendStatus(200);
+});
+
+app.post('/launchBackup', async (req, res) => {
+    setHeaders(res);
+
+    //await sleep(5000);
+
+    await launchBackup(req, res);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
--- backend/system/do_backup.sh
+++ backend/system/do_backup.sh
@@ -2,7 +2,7 @@
 
 ###################################################################
 # Script Name	: do_backup.sh
-# Description	: Copy the files (called by launch_backup.sh)
+# Description	: Copy the files (called by backup_launcher.sh)
 # Args        : The input and output folders, the files suffix,
 #               the media type and the device ID
 # Author      : Ahmed Abdeen
@@ -18,10 +18,25 @@ CONF_DIR=$MFB_CONF_DIR
 subFolders=()
 
 numberOfCopiedFiles=0
-lastBackupDateFileName="${CONF_DIR}/latest_${DEVICE_ID}_${MEDIA_TYPE}"
+lastBackupDateFileName="latest_${DEVICE_ID}_${MEDIA_TYPE}"
 
 # If the latest file contains nothing, put a very old date on it
-lastBackupDate=$(cat "$lastBackupDateFileName" 2>/dev/null || echo "1970-01-01")
+lastBackupDate=$(cat "$lastBackupDateFileName" 2>/dev/null) || {
+  printf "Cannot find the previous backup information file: %s\n" "$lastBackupDateFileName"
+  exit 1
+}
+
+[[ "$(echo "$lastBackupDate" | awk '{print length}')" -gt 0 ]] || {
+  echo "Cannot find the previous backup date"
+  exit 2
+}
+
+date -d "$lastBackupDate" +'%Y-%m-%d' 1>/dev/null || {
+  printf "The previous backup date is not a valid date (YYYY-MM-DD): %s\n" "$lastBackupDate"
+  exit 3
+}
+
+printf "Start date %s\n" "$lastBackupDate"
 
 while read -r tmp; do
   [[ "$(echo "$tmp" | awk '{print length}')" -gt 0 ]] || {
@@ -40,18 +55,20 @@ while read -r tmp; do
     subFolders+=("$subFolder")
   fi
 
-  echo -ne "Processing file: $file"\\r
-
-  cp -p "$p" "$subFolder" 2>/dev/null
+  cp -pn "$p" "$subFolder" 2>/dev/null || {
+    printf "File %s cannot be copied\n" "$p"
+    exit 4
+  }
 
   lastProcessedFile=$file
   ((numberOfCopiedFiles++))
 done <<<"$(find "$INPUT_FOLDER/" -type f -name "$FILE_SUFFIX" -newermt "$lastBackupDate" | sort)"
 
-echo "$numberOfCopiedFiles" > "$$.mfb.pid"
+echo "$numberOfCopiedFiles" >"$$.mfb.pid"
 
 # Update the latest backed up file
 if [[ "$(echo "$lastProcessedFile" | awk '{print length}')" -gt 0 ]]; then
   backup_date=$(date -d "$((${lastProcessedFile:4:8}))" +'%Y-%m-%d')
-  echo "$backup_date" > "$lastBackupDateFileName"
+  echo "$backup_date" >"$lastBackupDateFileName"
+  printf "Last processed file: %s\n" "$lastProcessedFile"
 fi
