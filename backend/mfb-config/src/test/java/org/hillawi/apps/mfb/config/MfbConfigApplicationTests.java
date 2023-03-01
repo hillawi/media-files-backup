444cc6a12ebc730865b0f030139bbfae28847631
Ahmed Hillawi <abdeenahmed2002@hotmail.com>
2023-03-01 19:28:50 +0100

    Working on docker + documentation update

--- backend/mfb-config/src/test/java/org/hillawi/apps/mfb/config/MfbConfigApplicationTests.java
+++ backend/mfb-config/src/test/java/org/hillawi/apps/mfb/config/MfbConfigApplicationTests.java
@@ -3,10 +3,8 @@ package org.hillawi.apps.mfb.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MfbConfigApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
