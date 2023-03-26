f834117abdfa66e7498a61cf6fdbcf836cc8aae9
Ahmed Hillawi <abdeenahmed2002@hotmail.com>
2023-03-26 17:51:51 +0200

    mfb-config: Updated spring boot

--- backend/mfb-config/src/test/java/org/hillawi/apps/mfb/config/MfbConfigApplicationTests.java
+++ backend/mfb-config/src/test/java/org/hillawi/apps/mfb/config/MfbConfigApplicationTests.java
@@ -1,11 +1,8 @@
 package org.hillawi.apps.mfb.config;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 class MfbConfigApplicationTests {
 
-	void contextLoads() {
-	}
+    void contextLoads() {
+    }
 
 }
